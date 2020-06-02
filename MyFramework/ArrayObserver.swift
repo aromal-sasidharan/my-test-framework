233bbb98a04e152fa91e73b26d2cc2a58e25bafb
Aromal <aromalsasidharan@gmail.com>
2020-06-02 21:22:05 +0800

    initial framework

---
+++ MyFramework/ArrayObserver.swift
@@ -0,0 +1,18 @@
+//
+//  ArrayObserver.swift
+//  MyFramework
+//
+//  Created by Leo on 2/6/20.
+//  Copyright © 2020 Leo. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public class ArrayObserver {
+    public func getObserver() -> Observable<[Int]> {
+        return Observable.deferred { () -> Observable<[Int]> in
+            return .just([1,2,3,4])
+        }
+    }
+}
